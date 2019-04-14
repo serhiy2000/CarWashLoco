5688fcd61eaa6b4f03cfeb2c8fef9536674078af
Serhiy Perepelyuk <3774217@gmail.com>
2019-04-14 19:26:14 +0300

    at last final version. some bugs removed.

--- src/Menu.java
+++ src/Menu.java
@@ -2,7 +2,9 @@ import java.util.*;
 
 public class Menu {
 
-    private static String CURRENCY = "uah";
+    private final static String CURRENCY = "uah";
+    static final String REGEXTOGETCHARS = "[^A-Za-z]";
+    static final String REGEXTOGETNUMBERS = "[^0-9]";
 
     private static String firstUpperCase(String word) {
         if (word == null || word.isEmpty()) return ""; 
@@ -12,10 +14,8 @@ public class Menu {
     static void start() {
         // creates menu for main entrance;
         Map<String, List<Services>> mainMenu = new LinkedHashMap<>();
-
         // fills menu with data from Services;
         Services.fillMainMenu(mainMenu);
-
         Set<Basket> bill = new LinkedHashSet<>();
         String input, key = "init";
         int roomMark = 1;
@@ -26,10 +26,8 @@ public class Menu {
 
         do {
             Scanner scanner = new Scanner(System.in);
-
             checker = false;
             checkerForWrongInput = false;
-
             if (roomMark == 1) System.out.print("Choose your vehicle type: \n" + mainMenu.keySet() + "\n");
             input=firstUpperCase(scanner.nextLine());
             Menu.clearScreen();
@@ -37,30 +35,30 @@ public class Menu {
             // shows servicesForSelectedVehicle list for the selected vehicle
             if (roomMark == 1){
                 for (String iteratorWithinKeySet : mainMenu.keySet()){
-                    convertToWordCommand = iteratorWithinKeySet.replaceAll("[^A-Za-z]","");
-                    convertToIntegerCommand = iteratorWithinKeySet.replaceAll("[^0-9]","");
-                    if ((input.equalsIgnoreCase(convertToWordCommand) || input.equals(convertToIntegerCommand))) key = iteratorWithinKeySet;
+                    convertToWordCommand = iteratorWithinKeySet.replaceAll(REGEXTOGETCHARS,"");
+                    convertToIntegerCommand = iteratorWithinKeySet.replaceAll(REGEXTOGETNUMBERS,"");
+                    if ((input.equalsIgnoreCase(convertToWordCommand) | input.equals(convertToIntegerCommand))) key = iteratorWithinKeySet;
                     if (roomMark == 1 & key.equals(iteratorWithinKeySet)){
                         roomMark = 2;
                         Menu.clearScreen();
-                        System.out.println("You've entered: " + key.toUpperCase().replaceAll("[^A-Za-z]",""));
-                        Menu.printTableWithServices(mainMenu,key);
                         checkerForWrongInput = true;
                     }
                 }
+                System.out.println("You've entered: " + key.toUpperCase().replaceAll(REGEXTOGETCHARS,""));
+                Menu.printTableWithServices(mainMenu,key);
             }
 
             // Check if input equals servicesForSelectedVehicle, than adds to the basket
             if (roomMark == 2 & !checkerForWrongInput){
                 for (int i = 0; mainMenu.get(key).size() > i; i++) {     // Check if input equals servicesForSelectedVehicle
                     Services servicesForSelectedVehicle = mainMenu.get(key).get(i);
-                    convertToWordCommand = servicesForSelectedVehicle.getService().replaceAll("[^A-Za-z]","");
-                    convertToIntegerCommand = servicesForSelectedVehicle.getService().replaceAll("[^0-9]","");
+                    convertToWordCommand = servicesForSelectedVehicle.getService().replaceAll(REGEXTOGETCHARS,"");
+                    convertToIntegerCommand = servicesForSelectedVehicle.getService().replaceAll(REGEXTOGETNUMBERS,"");
 
                     if (input.equalsIgnoreCase(convertToWordCommand) || input.equals(convertToIntegerCommand)) {
                         // addition to the basket
-                        System.out.println("You have ordered "+ mainMenu.get(key).get(i).getService().replaceAll("[^A-Za-z]","") +
-                                " (" + mainMenu.get(key).get(i).getDescription() + ") for "+key +".\n");
+                        System.out.println("You have ordered "+ mainMenu.get(key).get(i).getService().replaceAll(REGEXTOGETCHARS,"") +
+                                " (" + mainMenu.get(key).get(i).getDescription().replaceAll(REGEXTOGETCHARS,"") + ") for "+key.replaceAll(REGEXTOGETCHARS,"") +".\n");
                         Services addServices = mainMenu.get(key).get(i);
                         Basket.addServiceBasket(addServices, bill);
                         checker = true;
@@ -70,36 +68,36 @@ public class Menu {
                 }
             }
 
-            // incorrect input runover;
-            if (!(input.equalsIgnoreCase("back") |
-                    input.equalsIgnoreCase("bill") |
-                    input.equalsIgnoreCase("q") |
-                    input.equals(key) |
-                    checker |
-                    checkerForWrongInput)) {
-                System.out.println("Incorrect input. Please try again.");
-                if (roomMark == 2) Menu.printTableWithServices(mainMenu,key);
-            }
-
             // prints current bill;
             if (input.equalsIgnoreCase("bill")) Basket.billPrint(bill);
 
             // go one level up;
             if (input.equalsIgnoreCase("back")) roomMark = 1;
 
+            // incorrect input processing;
+            if (!(input.equalsIgnoreCase("back") |
+                    input.equalsIgnoreCase("bill") |
+                    input.equalsIgnoreCase("q") |
+                    input.equals(key) |
+                    checker |
+                    checkerForWrongInput)){
+                System.out.println("Incorrect input. Please try again.");
+                if (roomMark == 2) Menu.printTableWithServices(mainMenu,key);
+            }
         } while (!(input.equalsIgnoreCase("q")));
-
         System.out.println("Thank you.");
         Basket.billPrint(bill);
     }
 
     private static void clearScreen () {
         // emulation of clearscreen command
-        for (int i=0; i < 24; i++) System.out.println();
+        for (int i=0; i < 44; i++) System.out.println();
     }
-
     private static void printTableWithServices(Map<String,List<Services>> mainMenu, String key) {
-        System.out.println("We can offer following services for your: "+key.toUpperCase());
+        if (key.equalsIgnoreCase("INIT")){
+            System.out.println("You didn't enter correct command.");
+        } else {
+            System.out.println("We can offer following services for your: "+key.toUpperCase());
         String format = "|%1$-15s|%2$-5s|%3$-8s|%4$-70s|\n";
         int tableLength = 103;
         for(int i=1; i<=tableLength; i++) System.out.print("=");
@@ -107,13 +105,12 @@ public class Menu {
         System.out.format(format, "Service", "Price", "Currency", "Description");
         for(int i=1; i<=tableLength; i++) System.out.print("=");
         System.out.println();
-
         for (Services current : mainMenu.get(key)) {
-            System.out.format(format, current.getService(), current.getPrice(), CURRENCY, current.getDescription());
+                System.out.format(format, current.getService(), current.getPrice(), CURRENCY, current.getDescription());
         }
-
         for(int i=1; i<=tableLength; i++) System.out.print("=");
         System.out.println();
+        }
     }
 }
 
--- src/Services.java
+++ src/Services.java
@@ -40,16 +40,14 @@ public class Services {
     }
 
     private static void fillServicesForCar(List<Services> services){
-
-            services.add(new Services(CAR,"1.Wash", 20, "Full body wash with active foam"));
-            services.add(new Services(CAR,"2.Tires wash", 10, "Tires wash with water and active foam"));
-            services.add(new Services(CAR,"3.Engine wash", 15, "Engine wash with water-soluble engine degreaser"));
-            services.add(new Services(CAR,"4.Vacuum", 15, "Interior cleaning with the vacuum cleaner"));
-            services.add(new Services(CAR,"5.Polish", 25, "Polishing with an electric polisher and covering a car with a wax"));
+        services.add(new Services(CAR,"1.Wash", 20, "Full body wash with active foam"));
+        services.add(new Services(CAR,"2.Tires wash", 10, "Tires wash with water and active foam"));
+        services.add(new Services(CAR,"3.Engine wash", 15, "Engine wash with water-soluble engine degreaser"));
+        services.add(new Services(CAR,"4.Vacuum", 15, "Interior cleaning with the vacuum cleaner"));
+        services.add(new Services(CAR,"5.Polish", 25, "Polishing with an electric polisher and covering a car with a wax"));
         }
 
     private static void fillServicesForCross(List<Services> services){
-
         services.add(new Services(CROSS,"1.Wash", 25, "Full body wash with active foam"));
         services.add(new Services(CROSS,"2.Tires wash", 10, "Tires wash with water and active foam"));
         services.add(new Services(CROSS,"3.Engine wash", 20, "Engine wash with water-soluble engine degreaser"));
@@ -58,7 +56,6 @@ public class Services {
     }
 
     private static void fillServicesForMoto(List<Services> services){
-
         services.add(new Services(MOTO,"1.Wash", 10, "Full body wash with active foam"));
         services.add(new Services(MOTO,"2.Tires wash", 5, "Tires wash with water and active foam"));
         services.add(new Services(MOTO,"3.Engine wash", 15, "Engine wash with water-soluble engine degreaser"));
@@ -66,28 +63,22 @@ public class Services {
     }
 
     private static void fillServicesForTruck(List<Services> services){
-            services.add(new Services(TRUCK,"1.Wash", 50, "Full body wash with active foam"));
-            services.add(new Services(TRUCK,"2.Tires wash", 20, "Tires wash with water and active foam"));
-            services.add(new Services(TRUCK,"3.Engine wash", 25, "Engine wash with water-soluble engine degreaser"));
-            services.add(new Services(TRUCK,"4.Vacuum", 15, "Interior cleaning with the vacuum cleaner"));
-            services.add(new Services(TRUCK,"5.Polish", 30, "Polishing with an electric polisher and covering a truck with a wax"));
+        services.add(new Services(TRUCK,"1.Wash", 50, "Full body wash with active foam"));
+        services.add(new Services(TRUCK,"2.Tires wash", 20, "Tires wash with water and active foam"));
+        services.add(new Services(TRUCK,"3.Engine wash", 25, "Engine wash with water-soluble engine degreaser"));
+        services.add(new Services(TRUCK,"4.Vacuum", 15, "Interior cleaning with the vacuum cleaner"));
+        services.add(new Services(TRUCK,"5.Polish", 30, "Polishing with an electric polisher and covering a truck with a wax"));
         }
 
     static void fillMainMenu(Map<String, List<Services>> menuMap){
-
         List<Services> serviceForCar = new ArrayList<>();
         Services.fillServicesForCar(serviceForCar);
-
         List<Services> serviceForTruck = new ArrayList<>();
         Services.fillServicesForTruck(serviceForTruck);
-
         List<Services> serviceForMoto = new ArrayList<>();
         Services.fillServicesForMoto(serviceForMoto);
-
         List<Services> serviceForCross = new ArrayList<>();
         Services.fillServicesForCross(serviceForCross);
-
-
         menuMap.put(CAR, serviceForCar);
         menuMap.put(MOTO, serviceForMoto);
         menuMap.put(TRUCK, serviceForTruck);
